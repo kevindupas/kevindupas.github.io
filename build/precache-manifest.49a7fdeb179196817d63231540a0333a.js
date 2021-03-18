db9e5ec4dc08aff137250a2c0924344a0161b12b
Kévin Dupas <kdupas@planetworkint.com>
2021-03-18 18:58:38 +0100

    Commit

---
+++ build/precache-manifest.49a7fdeb179196817d63231540a0333a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bea9aae4ddaa1e765e1706c4b1270c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fbb9a1c0cb41fbf9a8a",
+    "url": "/static/css/main.843ee4d2.chunk.css"
+  },
+  {
+    "revision": "7289513f4a52a0b665dd",
+    "url": "/static/js/2.5bf608ba.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5bf608ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fbb9a1c0cb41fbf9a8a",
+    "url": "/static/js/main.4a2a6a49.chunk.js"
+  },
+  {
+    "revision": "840f97409b163973c9e2",
+    "url": "/static/js/runtime-main.a119cdfb.js"
+  },
+  {
+    "revision": "e316d44d173b5c10df022722cbd9eeb3",
+    "url": "/static/media/heart.e316d44d.svg"
+  },
+  {
+    "revision": "34df31112e5959a1e5c0abf66851818d",
+    "url": "/static/media/play.34df3111.svg"
+  },
+  {
+    "revision": "d3bbc6b13e942677b14259bb327d512f",
+    "url": "/static/media/plus.d3bbc6b1.svg"
+  }
+]);
